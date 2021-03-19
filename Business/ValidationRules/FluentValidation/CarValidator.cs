63f2bce9f8da4349ed119911678e15641b868cd6
Efecan <efecanbayat@gmail.com>
2021-03-19 22:00:23 +0300

    CarValidator' da ufak değişiklik

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -14,7 +14,7 @@ namespace Business.ValidationRules.FluentValidation
             RuleFor(c => c.Description).MaximumLength(2);
             RuleFor(c => c.DailyPrice).NotEmpty();
             RuleFor(c => c.DailyPrice).GreaterThan(0);
-            RuleFor(c => c.ModelYear).LessThan(DateTime.Now.Year);
+            RuleFor(c => c.ModelYear).LessThanOrEqualTo(DateTime.Now.Year);
             RuleFor(c => c.ModelYear).GreaterThan(1886);
         }
     }
