1d913236213591d4c13d05339f0e9b26472a0f23
Efecan <efecanbayat@gmail.com>
2021-03-19 18:54:04 +0300

    12.Ders 2.Ödev FluentValidation

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,8 +1,11 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -19,11 +22,9 @@ namespace Business.Concrete
         }
 
         public IResult Add(Brand brand)
-        {
-            if (brand.Name.Length < 2)
-            {
-                return new ErrorResult(Messages.BrandNameInvalid);
-            }
+        {           
+            ValidationTool.Validate(new BrandValidator(), brand);
+            
             _brandDal.Add(brand);
             return new SuccessResult(Messages.BrandAdded);
         }
